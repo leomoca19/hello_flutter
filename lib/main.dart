82441f25315df752c566bf47d95e8069c7e3d9b7
leomoca19 <leomoca19@gmail.com>
2024-02-16 09:46:51 -0500

    Initial setup

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'hello_flutter',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    print("Hello World");
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea:'),
+          Text(appState.current.asString),
+
+          ElevatedButton(
+            onPressed: () {
+              print('someone pressed the button');
+            },
+            child: Text('I am a button'),
+          )
+        ],
+      ),
+    );
+  }
+}
